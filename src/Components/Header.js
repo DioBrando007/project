2cfaeb624deff65dec6850edfe2106bb91a252c5
Zhambyl Kalymbet <ZKalymbet@szhuldyz.kz>
2022-06-20 20:07:13 +0600

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import classes from'./Header.module.css'
+
+const Header = () => {
+    return (
+        <header className={classes.header}>
+        <img src='https://i.pinimg.com/736x/1b/b5/2d/1bb52de966fa78afc9e72e1254e01c54.jpg'/>
+      </header>
+    )
+}
+
+export default Header;
